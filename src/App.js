ccdee3ebbb7fa9eea20a824cc5d869034a39d449
rodneyrinaldi <rodneyrinaldi@hotmail.com>
2020-06-11 22:22:44 -0300

    send email routine

--- src/App.js
+++ src/App.js
@@ -11,18 +11,41 @@ function App() {
 
   function handleSubmit(e) {
     e.preventDefault();
-    const parameters = {
+
+    const contactpar = {
+      "sitename": process.env.REACT_APP_SITE_NAME,
+      "emailcontact": process.env.REACT_APP_SITE_EMAIL,
+      "emailaddress": email
+    };
+    sendEmail(
+      process.env.REACT_APP_SERVICE_ID,
+      process.env.REACT_APP_TEMPLATE_CONTACT,
+      contactpar,
+      process.env.REACT_APP_USER_ID
+    );
+
+    const answerpar = {
       "sitename": process.env.REACT_APP_SITE_NAME,
       "emailaddress": email
     };
-    console.log(parameters);
-    emailjs.send(
+    sendEmail(
       process.env.REACT_APP_SERVICE_ID,
-      process.env.REACT_APP_TEMPLATE_ID,
-      parameters,
+      process.env.REACT_APP_TEMPLATE_ANSWER,
+      answerpar,
       process.env.REACT_APP_USER_ID
+    );
+  }
+
+  function sendEmail(service, emailtemplate, parameters, user) {
+    emailjs.send(
+      service,
+      emailtemplate,
+      parameters,
+      user
     ).then((result) => {
       console.log(result.text);
+      setEmail();
+      document.getElementById("EmailID").innerHTML = "";
     }, (error) => {
       console.log(error.text);
     });
@@ -35,7 +58,7 @@ function App() {
         <img src={rrlogo} className="Logorr" alt="rrlogo" />
         <form onSubmit={handleSubmit}>
           <p className="Message">Novidades em breve, inscreva-se para recebê-las</p>
-          <input type="email" name="Email" placeholder="E-mail" required className="Email" onChange={e => setEmail(e.target.value)} />
+          <input type="email" id="EmailID" name="Email" placeholder="E-mail" required className="Email" onChange={e => setEmail(e.target.value)} />
           <input type="submit" value="ENVIAR" className="Button" />
         </form>
       </header>
